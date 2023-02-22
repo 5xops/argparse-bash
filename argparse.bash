d6056a2e7ce3ba6200cd623c9a8f55aa40cc1ad7
itxx00 <itxx00@gmail.com>
2023-02-22 22:23:59 +0800

    Update argparse.bash

--- argparse.bash
+++ argparse.bash
@@ -11,26 +11,23 @@
 #
 # https://github.com/nhoffman/argparse-bash
 # MIT License - Copyright (c) 2015 Noah Hoffman
+# https://github.com/5xops/argparse-bash
+# MIT License - Copyright (c) 2023 5xops
 
 argparse(){
     argparser=$(mktemp 2>/dev/null || mktemp -t argparser)
     cat > "$argparser" <<EOF
+# coding: utf-8
 from __future__ import print_function
 import sys
 import argparse
-import os
-
-
 class MyArgumentParser(argparse.ArgumentParser):
     def print_help(self, file=None):
         """Print help and exit with error"""
         super(MyArgumentParser, self).print_help(file=file)
         sys.exit(1)
-
-parser = MyArgumentParser(prog=os.path.basename("$0"),
-            # preserve newlines in description
-            formatter_class=argparse.RawDescriptionHelpFormatter,
-            description="""$ARGPARSE_DESCRIPTION""")
+parser = MyArgumentParser(prog="$PROCNAME" + " $ACTION",
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 EOF
 
     # stdin to this function should contain the parser definition
@@ -41,9 +38,10 @@ args = parser.parse_args()
 for arg in [a for a in dir(args) if not a.startswith('_')]:
     key = arg.upper()
     value = getattr(args, arg, None)
-
-    if isinstance(value, bool) or value is None:
-        print('{0}="{1}";'.format(key, 'yes' if value else ''))
+    if isinstance(value, bool):
+        print('{0}="{1}";'.format(key, 'yes' if value else 'no'))
+    elif value is None:
+        print('{0}="{1}";'.format(key, ''))
     elif isinstance(value, list):
         print('{0}=({1});'.format(key, ' '.join('"{0}"'.format(s) for s in value)))
     else:
@@ -53,15 +51,17 @@ EOF
     # Define variables corresponding to the options if the args can be
     # parsed without errors; otherwise, print the text of the error
     # message.
-    if python "$argparser" "$@" &> /dev/null; then
-        eval $(python "$argparser" "$@")
+    if $PYTHON "$argparser" "$@" &> /dev/null; then
+        set -o noglob
+        eval $($PYTHON "$argparser" "$@")
+        set +o noglob
         retval=0
     else
-        python "$argparser" "$@"
+        $PYTHON "$argparser" "$@"
         retval=1
     fi
 
-    rm "$argparser"
+    /bin/rm -f "$argparser"
     return $retval
 }
 
